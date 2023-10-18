3f2c3a50da4c7c187990cb30106f9d4c4dbdd307
Nugget-Meister <tremainecamacho@gmail.com>
2023-10-18 18:42:27 -0400

    appicalls.js now has base code, needs testing

---
+++ src/helpers/apicalls.js
@@ -0,0 +1,59 @@
+// Remember to refactor the api url in the .env file before you submit/deploy page!!!!
+
+const URL = import.meta.env.VITE_API_URL
+
+const getAllTransactions= () => {
+    return fetch(URL)
+    .then(res => res.json())
+    .catch(err => console.error(err))
+    
+}
+
+const getTransaction = (id) => {
+    return fetch(`${URL}/${id}`)
+    .then(res => res.json())
+    .catch(err => console.error(err))
+}
+
+const addTransaction = (id, data) => {
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {"Content-Type": "application/json"}
+    }
+
+    return fetch(URL, options)
+    .then(res => res.json())
+    .catch(err => console.error(err))     
+}
+
+const updateTransaction = (id, data) => {
+    const options = {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: {"Content-Type": "application/json"}
+    }
+
+    return fetch(`${URL}/${id}`, options)
+    .then(res => res.json())
+    .catch(err => console.error(err)) 
+
+}
+
+const deleteTransaction = (id) => {
+    const options = {
+        method: "DELETE"
+    }
+
+    return fetch(URL, options)
+    .then(res => res.json())
+    .catch(err => console.error(err)) 
+}
+
+module.exports = {
+    getAllTransactions,
+    getTransaction,
+    addTransaction,
+    updateTransaction,
+    deleteTransaction
+}
