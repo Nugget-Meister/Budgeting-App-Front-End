79f0b32c4ab727d32e4d41e2eb2b0cb6b36cad7c
Nugget-Meister <tremainecamacho@gmail.com>
2023-10-17 21:41:37 -0400

    Working links for Navbar.jsx, prototype Show.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import React from "react";
+
+import NavBar from "./components/common/NavBar";
+
 import Index from "./components/Index/Index";
+import NewForm from "./components/Form/NewForm";
+import EditForm from "./components/Form/EditForm";
+import Show from "./components/Show/Show";
 
 // import './App.css'
 
@@ -11,11 +17,12 @@ function App() {
     <>
       <Router>
         <main>
+          <NavBar/>
           <Routes>
             <Route path="/" element={<Index/>}/>
-            <Route path="/New" element={<Index/>}/>
-            <Route path="/:id/edit" element={<Index/>}/>
-            <Route path="/:id" element={<Index/>}/>
+            <Route path="/new" element={<NewForm/>}/>
+            <Route path="/:id/edit" element={<EditForm/>}/>
+            <Route path="/:id" element={<Show/>}/>
           </Routes>
         </main>
       </Router>
--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <div>
-            
+       
+           <Link to="/"> Bank of Githubbica</Link>
+           <Link to="/new"> Nu</Link>
+           
         </div>
     );
 }
